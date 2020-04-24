fafa2a852e0ed6f80b0445906c11d79eaead61f1
y0vimal <mailtovimal87@gmail.com>
2020-04-24 17:05:11 +0530

    @Before and @After Methods

--- src/test/java/MathUtils/MathUtilsTest.java
+++ src/test/java/MathUtils/MathUtilsTest.java
@@ -1,14 +1,46 @@
 package MathUtils;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
-    private MathUtils mathUtils = new MathUtils();
+
+    private MathUtils mathUtils;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MathUtilsTest.class);
+
+    /*
+    * Junit create separte instance of test class for each test - method
+    * but @BeforeAll method shall be excuted prior to test call instantiation
+    * Therefore this method should be static
+    * same is for @AfterAll method
+    * */
+    @BeforeAll
+    static void beforeAll(){
+        LOGGER.info("before all ... ");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        LOGGER.info("after all ... ");
+    }
+
+    @BeforeEach
+    void beforeEachMethod(){
+        mathUtils = new MathUtils();
+    }
+
+    void afterEachMethod(){
+            LOGGER.info("after each method ... ");
+    }
 
     @Test
-    public void testAdd(){
+     void testAdd(){
 
         assertEquals(5, mathUtils.add(1, 4));
         assertNotEquals(5, mathUtils.add(1, 3));
@@ -16,6 +48,7 @@ class MathUtilsTest {
 
     @Test
     void testDivide(){
+
         assertEquals(2, mathUtils.divide(5, 2));
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide a by b");
 
