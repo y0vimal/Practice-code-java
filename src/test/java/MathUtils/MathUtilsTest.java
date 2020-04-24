6211461678ef4cd3969149db4138860eab26e7ab
y0vimal <mailtovimal87@gmail.com>
2020-04-24 18:02:10 +0530

    adding @DisplayName, @Disabled, assumeTrue(), @Nested annotations and methods

--- src/test/java/MathUtils/MathUtilsTest.java
+++ src/test/java/MathUtils/MathUtilsTest.java
@@ -1,9 +1,6 @@
 package MathUtils;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -38,20 +35,66 @@ class MathUtilsTest {
     void afterEachMethod(){
             LOGGER.info("after each method ... ");
     }
-
+        /*
+        * @Disable is used to disable a method from execution
+        * */
     @Test
-     void testAdd(){
+    @DisplayName("tesing add method utility")
 
+     void testAdd(){
+        /*
+        * assumption is used when we are assuming something must be true to run this test case
+        * if assumptions is false this test is disabled
+        * */
+
+        /*
+        * assertAll() can be used for multiple assertions to run at once
+        * */
+
+        assertAll(
+                () ->assertEquals(5, mathUtils.add(1, 4)),
+                () ->assertNotEquals(5, mathUtils.add(1, 3))
+        );
+        /*assumeTrue(true);
         assertEquals(5, mathUtils.add(1, 4));
-        assertNotEquals(5, mathUtils.add(1, 3));
+        assertNotEquals(5, mathUtils.add(1, 3));*/
     }
 
     @Test
+    @Disabled
+    @DisplayName("Testing division utility ")
     void testDivide(){
 
         assertEquals(2, mathUtils.divide(5, 2));
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide a by b");
 
     }
+    /*
+    * @Nested annotation is used to group multiple but similar test cases
+    * for E.g. both the below methods testPositiveAddition() & testNegativeAddition()
+    * can be grouped using Nested class
+    * */
+    @Nested
+    class TestAddition{
+        @Test
+        void testPositiveAddition(){
+            assertEquals(5, mathUtils.add(1, 4));
+        }
+
+        @Test
+        void testNegativeAddition(){
+            assertEquals(-5, mathUtils.add(-1, -4));
+        }
+    }
+/*    @Test
+    void testPositiveAddition(){
+        assertEquals(5, mathUtils.add(1, 4));
+    }
+
+    @Test
+    void testNegativeAddition(){
+        assertEquals(-5, mathUtils.add(-1, -4));
+    }*/
+
 
 }
